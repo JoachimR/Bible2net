7fc3c4496781e1817add5d4a076a3a7992fb7449
JoachimReiss <reiss@bryter.io>
2021-09-07 22:51:58 +0200

    gradle/kotlin fixes

--- twdparser/src/main/java/de/reiss/bible2net/theword/twdparser/Util.kt
+++ twdparser/src/main/java/de/reiss/bible2net/theword/twdparser/Util.kt
@@ -11,24 +11,12 @@ import java.util.Date
 private fun simpleDateFormat() = SimpleDateFormat("yyyy-MM-dd") // use function for thread safety
 
 fun dateFromString(dateString: String?): Date? {
-    return try {
-        simpleDateFormat().parse(dateString)
+    try {
+        if (dateString != null) {
+            return simpleDateFormat().parse(dateString)
+        }
     } catch (e: Exception) {
         logWarn(e) { "Error when trying to parse date from string $dateString" }
-        null
     }
-}
-
-fun flattedDateFromString(dateString: String?): Date? {
-    return try {
-        Calendar.getInstance().apply {
-            time = simpleDateFormat().parse(dateString)
-            set(Calendar.HOUR_OF_DAY, 0)
-            set(Calendar.MINUTE, 0)
-            set(Calendar.SECOND, 0)
-            set(Calendar.MILLISECOND, 0)
-        }.time
-    } catch (e: ParseException) {
-        null
-    }
+    return null
 }
